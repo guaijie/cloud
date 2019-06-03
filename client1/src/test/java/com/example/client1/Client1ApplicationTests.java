1205434b71a389b59cf597994779ab34e2c52e0c
null <null>
2019-06-03 19:41:21 +0800

    update files

--- client1/src/test/java/com/example/client1/Client1ApplicationTests.java
+++ client1/src/test/java/com/example/client1/Client1ApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.eureka;
+package com.example.client1;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class EurekaApplicationTests {
+class Client1ApplicationTests {
 
     @Test
     void contextLoads() {
